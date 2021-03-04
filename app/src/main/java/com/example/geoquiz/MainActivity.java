3ae644276907c136b556f4dd96a2d410e63239fd
Vlad Bezdolniy <spiring110@ukr.net>
2021-03-04 20:35:06 +0200

    country list

--- app/src/main/java/com/example/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -9,22 +9,26 @@ import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.View;
 
+import android.widget.ListView;
 import android.widget.TextView;
 
 
-
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.CountDownLatch;
 
 public class MainActivity extends AppCompatActivity {
     GeoTimer geo;
     TextView timeView;
-
+    String[] countries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         countries = getResources().getStringArray(R.array.countries);
+
 
     }
 
@@ -50,13 +54,57 @@ public class MainActivity extends AppCompatActivity {
 
         TextView booleanTest = findViewById(R.id.bool_test);
 
+
         if(GeoTimer.isRun){
-            booleanTest.setText("True");
+            booleanTest.setText(new Country(countries).name);
         }
         else{
             booleanTest.setText("False");
         }
 
+    }
+
+}
+
+class Country{
+
+    String name;
+    char[] encryptedNameList;
+
+    Country(String[] countries){
+        this.name = countries[new RandomCountryNum(countries.length).getNum()];
+    }
+
+    void encrypting(){
+
+    }
+
+}
+
+class RandomCountryNum{
+    private final int num;
+
+    RandomCountryNum(int arraySize){
+
+        num = numGenerator(Player.getRank(), arraySize);
+    }
+
+    int numGenerator(int rank, int arraySize){
+        Random random = new Random();
+
+        return random.nextInt(arraySize); // Change!
+    }
+
+    int getNum(){
+        return num;
+    }
+}
+
+class Player{
+    private static int rank;
+
+    static int getRank(){
+        return rank;
     }
 
 }
