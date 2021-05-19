eaee625171c8a4e1e09a0e98022c5a24dabee87d
Vlad Bezdolniy <spiring110@ukr.net>
2021-05-19 21:54:25 +0300

    cicle animation

--- app/src/main/java/com/example/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -1,19 +1,26 @@
 package com.example.geoquiz;
 
+import androidx.annotation.DrawableRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.View;
 
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Animation.AnimationListener;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 
@@ -27,22 +34,81 @@ public class MainActivity extends AppCompatActivity {
     TextView timeView;
     String[] countries;
 
+    //test
+    private ImageView[] countries_image_list;
+    private ImageView countryTeg;
+    private Animation animation;
+    private Random random = new Random();
+    int imageId;
+    //test
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         countries = getResources().getStringArray(R.array.countries);
+        countries = getResources().getStringArray(R.array.countries);
 
-         Button buttonStartGame = findViewById(R.id.button_1);
+
+
+
+        //Animation>
+        Animation.AnimationListener animationListener = new Animation.AnimationListener() {
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                countryTeg.postOnAnimation(new Runnable() {
+                    @Override
+                    public void run() {
+                        int runNum = random.nextInt(8) + 1;
+                        String uri = "@drawable/country_image_0" + runNum;
+                        imageId = getResources().getIdentifier(uri, null, getPackageName());
+                        countryTeg.setImageResource(imageId);
+                        countryTeg.startAnimation(animation);
+                    }
+                });
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+        };
+
+        animation = AnimationUtils.loadAnimation(this, R.anim.maping_animation);
+        animation.setAnimationListener(animationListener);
+        countryTeg = findViewById(R.id.country_tag);
+        countryTeg.startAnimation(animation);
+        //Animation>
+
+
     }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        countryTeg.clearAnimation();
+    }
+
+
+
 
 
     public void startGame(View view) {
         Intent intent = new Intent(this, PlayGround.class);
         startActivity(intent);
     }
+
+
 }
+
+
+
+
 
 class Country{
 
@@ -59,7 +125,8 @@ class Country{
         int maxCharacterNum = 14;
 
         if (name.length() > maxCharacterNum - 3){
-            System.out.println("Ошибка. Невозможно создать страну с таким количеством символов."); // Изменить на алерт и выход из приложения
+            System.out.println("Ошибка. Невозможно создать страну с таким количеством символов.");
+            // !!!Изменить на алерт и выход из приложения
         }
         else{
             encryptedNameList = new char[name.length() + 3 + random.nextInt(maxCharacterNum - (name.length() + 1))];
