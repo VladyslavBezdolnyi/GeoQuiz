44a263c4607fc4204320936c167abe262363fc72
Vlad Bezdolniy <spiring110@ukr.net>
2021-05-20 20:04:55 +0300

    refr

---
+++ app/src/main/java/com/example/geoquiz/Country.java
@@ -0,0 +1,43 @@
+package com.example.geoquiz;
+
+import java.util.Random;
+
+class Country{
+
+    String name;
+    char[] encryptedNameList;
+
+    Country(String[] countries){
+        this.name = countries[new RandomCountryNum(countries.length).getNum()];
+        encrypting(); // encrypting name char letters
+    }
+
+    private void encrypting(){
+        Random random = new Random();
+        int maxCharacterNum = 14;
+
+        if (name.length() > maxCharacterNum - 3){
+            System.out.println("Ошибка. Невозможно создать страну с таким количеством символов.");
+            // !!!Изменить на алерт и выход из приложения
+        }
+        else{
+            encryptedNameList = new char[name.length() + 3 + random.nextInt(maxCharacterNum - (name.length() + 1))];
+            // Add current letters
+            for (int i = 0; i < name.length(); i++){
+                encryptedNameList[i] = name.charAt(i);
+            }
+            // Generate random letters
+            for (int i = name.length(); i < encryptedNameList.length; i++){
+                encryptedNameList[i] = (char)(random.nextInt(26) + 'a');
+            }
+            // Swap letters in random order
+            for(int i=0; i < encryptedNameList.length - 1; i++){
+                int j = random.nextInt(encryptedNameList.length-1);
+                char temp = encryptedNameList[i];
+                encryptedNameList[i] = encryptedNameList[j];
+                encryptedNameList[j] = temp;
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/example/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -94,9 +94,11 @@ public class MainActivity extends AppCompatActivity {
         countryTeg.clearAnimation();
     }
 
-
-
-
+    @Override
+    protected void onStop() {
+        super.onStop();
+        countryTeg.clearAnimation();
+    }
 
     public void startGame(View view) {
         Intent intent = new Intent(this, PlayGround.class);
@@ -110,45 +112,9 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-class Country{
 
-    String name;
-    char[] encryptedNameList;
 
-    Country(String[] countries){
-        this.name = countries[new RandomCountryNum(countries.length).getNum()];
-        encrypting(); // encrypting name char letters
-    }
 
-    private void encrypting(){
-        Random random = new Random();
-        int maxCharacterNum = 14;
-
-        if (name.length() > maxCharacterNum - 3){
-            System.out.println("Ошибка. Невозможно создать страну с таким количеством символов.");
-            // !!!Изменить на алерт и выход из приложения
-        }
-        else{
-            encryptedNameList = new char[name.length() + 3 + random.nextInt(maxCharacterNum - (name.length() + 1))];
-            // Add current letters
-            for (int i = 0; i < name.length(); i++){
-                encryptedNameList[i] = name.charAt(i);
-            }
-            // Generate random letters
-            for (int i = name.length(); i < encryptedNameList.length; i++){
-                encryptedNameList[i] = (char)(random.nextInt(26) + 'a');
-            }
-            // Swap letters in random order
-            for(int i=0; i < encryptedNameList.length - 1; i++){
-                int j = random.nextInt(encryptedNameList.length-1);
-                char temp = encryptedNameList[i];
-                encryptedNameList[i] = encryptedNameList[j];
-                encryptedNameList[j] = temp;
-            }
-        }
-    }
-
-}
 
 class RandomCountryNum{
     private final int num;
